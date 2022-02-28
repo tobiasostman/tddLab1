d8c15d5e81ee552ccad19412224a4e07680fa0fe
TobiasOstman <tobias.ostman@gambitgroup.fi>
2022-02-28 16:30:31 +0200

    Step 1 completed tests for empty string, string containing just a number and 1+2

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,2 +1,11 @@
+import java.util.Arrays;
+
 public class StringCalculator {
+    public int add(String numbers) {
+        if (!numbers.isEmpty()) {
+            return Arrays.stream(numbers.split(","))
+                    .map(number -> Integer.parseInt(number)).reduce(Integer::sum).orElse(0);
+        }
+        return 0;
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,32 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class StringCalculatorTest {
+
+    StringCalculator stringCalculator = new StringCalculator();
+
+    @Test
+    @DisplayName("Empty string should return zero")
+    public void emptyStringShouldReturnZero() {
+        int result = stringCalculator.add("");
+        assertEquals(0, result);
+    }
+
+    @Test
+    @DisplayName("One plus nothing should return one")
+    public void OneShouldReturnOne(){
+        int result = stringCalculator.add("1");
+        assertEquals(1,result);
+    }
+
+    @Test
+    @DisplayName("Two plus one should return 3")
+    public void TwoPlusOneShouldReturnThree() {
+        int result = stringCalculator.add("2,1");
+        assertEquals(3,result);
+    }
+
 }
